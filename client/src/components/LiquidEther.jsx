8a92d77e3b28f1a38d99a0f544cb0bf6052ca20d
cindwei <cindywei@mit.edu>
2026-01-28 02:33:53 -0500

    cited reactbits inline code

--- client/src/components/LiquidEther.jsx
+++ client/src/components/LiquidEther.jsx
@@ -2,6 +2,7 @@ import { useEffect, useRef } from 'react';
 import * as THREE from 'three';
 import './LiquidEther.css';
 
+//citation: from reactbits liquidether
 export default function LiquidEther({
   mouseForce = 20,
   cursorSize = 100,
